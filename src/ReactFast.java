53f6a3a89dddb22aa7ebacd0a115700a218f65fb
Goga iremashvili <goga.iremashvili2000@gmail.com>
2020-01-20 20:30:19 +0400

    first commit

---
+++ src/ReactFast.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class ReactFast {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		
+		System.out.println("3");
+		Thread.sleep(1000);
+		System.out.println("2");
+		Thread.sleep(1000);
+		System.out.println("1");
+		System.out.println("Go!!!!!!!!!");
+		long startTime=System.currentTimeMillis();
+		
+		Scanner s=new Scanner(System.in);
+		String a=s.next();
+		System.out.println(a);
+		long stopTime=System.currentTimeMillis();
+		
+		long rectionTime=stopTime-startTime;
+		System.out.println(rectionTime+"ms");
+		
+		//C:\Users\admin\eclipse-workspace\Reaction Timer
+		
+	}
+
+}
